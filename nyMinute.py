bebb6d0577825eb8760e97b91d629eaffc331c7a
CrumplePunch <amarquez727@gmail.com>
2013-09-28 19:26:07 -0400

    Added Beautiful Soup BABY!

--- nyMinute.py
+++ nyMinute.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 
 import os
 
@@ -10,6 +11,14 @@ def home():
     
 
     return render_template('test.html')
+
+@app.route('/findArticles', methods= ['POST'])
+def findArticles():
+    timeWindow = int(request.form.get('time'))
+    topic = request.form.get('topic').replace(' ', '+')
+
+    return render_template('find.html', time = timeWindow, topic = topic)
+    
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
