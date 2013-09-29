b6d194d4eec258fce856b0eb9629c71afc9f798a
Joyce Wang <joycewang2002@yahoo.com>
2013-09-29 05:15:25 -0400

    fixed shit

--- static/js/test.js
+++ static/js/test.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(".next.button").click(function(){
-        alert("hello");
         var topic = $(".topic").val();
             if(topic != ""){
             $("#read").addClass('downTranslate');
