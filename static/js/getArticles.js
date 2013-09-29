a0837423fb0026aabef879ba4d141ddb2682fafa
Joyce Wang <joycewang2002@yahoo.com>
2013-09-29 01:18:07 -0400

    pushing test stuff

--- static/js/getArticles.js
+++ static/js/getArticles.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $(".submit").click(function(){
+        var topic = $(".topic").val();
+        alert(topic);
+        getArticleArray(topic);
+    });
+});
+
 function getArticleArray(topic){
 
     var articleArr;
@@ -5,15 +13,18 @@ function getArticleArray(topic){
         dataType: 'jsonp',
         jsonp: 'callback',
         jsonpCallback : 'svc_search_v2_articlesearch',
-        url:'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q=' + topic + '&sort=newestapi-key=979d61de2e40719bc34dd2fe25f7da0b:6:67932469',
+        url:'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q=' + topic + '&sort=newest&api-key=979d61de2e40719bc34dd2fe25f7da0b:6:67932469',
         success: function(resp){
             articleArr = resp.response.docs;
             $.each(articleArr,function(key,value){
                 var url = "?article=" + value.web_url;
                 articleArr[key] = url;
             });
-            console.log(articleArr);
         }
     });
     return articleArr;
+}
+
+function changeURL(){
+    alert(articleArr);
 }
